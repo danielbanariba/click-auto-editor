2be152c11698a03b06ae286207ecff0d794d9f19
Daniel Banariba <danielbanariba@protonmail.com>
2024-05-29 21:15:16 -0600

    Alv, youtube cambio la inpunacion xd

---
+++ 10.1. inpunar_video_V2.py
@@ -0,0 +1,6 @@
+import pyautogui
+import time
+
+# Tener un arreglo con las coordenadas ya predefinidas para hacerle click, cada coordenada va estar enumerada con el nombre
+# Entonces cuando ponga cuantas canciones deseo apelar, va agarar el numero de esas coordenadas, ejemplo
+# Tengo un arreglo con 10 coordenadas, si pongo 5, va a agarrar las primeras 5 coordenadas y va a hacerle click 
