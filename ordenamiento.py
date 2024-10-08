59a7f30150f3a56764bef3af9106e137d9eea192
Daniel Banariba <banaribad@gmail.com>
2024-10-08 02:12:21 -0600

    Refactorizando el codigo

--- ordenamiento.py
+++ ordenamiento.py
@@ -1,11 +1,11 @@
 # Abre el archivo en modo de lectura
-with open('lista_sello.txt', 'r', encoding='utf-8') as f:
+with open('bandas-subidas-al-canal.txt', 'r', encoding='utf-8') as f:
     nombres = f.readlines()
 
 # Ordena la lista en orden alfabético y elimina los nombres duplicados
 nombres = sorted(set(nombres))
 
 # Abre el archivo original en modo de escritura
-with open('lista_sello.txt', 'w', encoding='utf-8') as f:
+with open('bandas-subidas-al-canal.txt', 'w', encoding='utf-8') as f:
     for nombre in nombres:
         f.write(nombre)
--- scroll_prueba.py
+++
@@ -1,5 +0,0 @@
-import pyautogui
-import time
-
-pyautogui.click(2679, 525)
-pyautogui.scroll(-150)
