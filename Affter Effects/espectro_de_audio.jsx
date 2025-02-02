783389a9a63c8a27157d93e06356214717e614b3
Daniel Banariba <banaribad@gmail.com>
2025-02-01 21:28:56 -0600

    Se redujo el espectro al 50%

--- Affter Effects/espectro_de_audio.jsx
+++ Affter Effects/espectro_de_audio.jsx
@@ -11,11 +11,11 @@ var spectrumEffect = spectrumLayer.property("Effects").addProperty("Audio Spectr
 spectrumEffect.property("Audio Layer").setValue(audioLayer.index);
 spectrumEffect.property("Start Frequency").setValue(700);
 spectrumEffect.property("End Frequency").setValue(9100);
-spectrumEffect.property("Start Point").setValue([864.0, 2060.0]);
-spectrumEffect.property("End Point").setValue([864.0, 32]);
-spectrumEffect.property("Frequency bands").setValue(760);
-spectrumEffect.property("Maximum Height").setValue(32000);
-spectrumEffect.property("Thickness").setValue(7);
-spectrumEffect.property("Softness").setValue(0); // 0%
-spectrumEffect.property("Display Options").setValue(1); // Digital
-spectrumEffect.property("Side Options").setValue(1); // Side A
+spectrumEffect.property("Start Point").setValue([432.0, 1030.0]);  // Reducido a la mitad de [864.0, 2060.0]
+spectrumEffect.property("End Point").setValue([432.0, 16]);        // Reducido a la mitad de [864.0, 32]
+spectrumEffect.property("Frequency bands").setValue(380);          // Reducido a la mitad de 760
+spectrumEffect.property("Maximum Height").setValue(16000);         // Reducido a la mitad de 32000
+spectrumEffect.property("Thickness").setValue(4);                  // Reducido a la mitad de 7
+spectrumEffect.property("Softness").setValue(0);
+spectrumEffect.property("Display Options").setValue(1);
+spectrumEffect.property("Side Options").setValue(1);
